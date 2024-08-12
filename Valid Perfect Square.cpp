c162e3486f140f8857139958f8ea2cadd96da6de
KASHISH SHARMA <95432071+Kashish9086@users.noreply.github.com>
2024-08-12 14:34:37 +0530

    Create Valid Perfect Square.cpp

---
+++ Valid Perfect Square.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+#include<math.h>
+   
+
+bool isPerfectSquare(long long n) {
+	// Write your code here.
+	
+ 
+        long long x = sqrt(n);
+        
+        return (x * x == n);
+}
